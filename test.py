c5722ade4c486b05b6638288e2486d077d044538
IT22584854-Shehan <122781054+IT22584854@users.noreply.github.com>
2025-01-12 21:22:03 +0530

    test file

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+import app
+
+class BasicTestcase(unittest.TestCase):
+    def test_home(self):
+        
+        tester = app.app.test_client(self)
+
+        response = tester.get('/', content_type="html/text")
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn(b"Hello", response.data)
+
+if __name__ == "__main__":
+    unittest.main()
