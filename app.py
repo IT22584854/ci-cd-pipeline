29b7a00169578c273318eac3e6c0d2ac402409c4
IT22584854-Shehan <122781054+IT22584854@users.noreply.github.com>
2025-01-13 14:28:27 +0530

    new

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello from CI/CD powerd Flask app!"
+    return "Hello from CI/CD powerd Flask app! v2"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
